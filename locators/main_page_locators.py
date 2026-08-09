306ce87b44ed83f727c8b24be8e0420a618fad91
Irina Kozlova <irina.kozlova@vk.team>
2026-08-10 02:17:24 +0300

    comments update

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -2,21 +2,21 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    # НАВИГАЦИЯ - из вашего рабочего кода
+    # НАВИГАЦИЯ
     CONSTRUCTOR_BUTTON = (By.XPATH, '//p[text()="Конструктор"]/parent::a')
     ORDERS_LIST_BUTTON = (By.XPATH, '//p[text()="Лента Заказов"]/parent::a')  # ЗАГЛАВНАЯ "З"
     LOGIN_BUTTON_MAIN = (By.XPATH, ".//button[text()='Войти в аккаунт']")
     PROFILE_BUTTON = (By.XPATH, ".//p[text()='Личный Кабинет']")
     MAIN_LIST_TITLE = (By.XPATH, "//h1[text()='Соберите бургер']")
     
-    # ИНГРЕДИЕНТЫ - из вашего рабочего кода
+    # ИНГРЕДИЕНТЫ
     BUN_INGREDIENT = (By.XPATH, '//p[text()="Флюоресцентная булка R2-D3"]')
     INGREDIENT_DETAILS_POPUP = (By.XPATH, '//h2[text()="Детали ингредиента"]')
     CROSS_BUTTON = (By.XPATH, '//button[contains(@class,"close")]')
     INGREDIENT_COUNTER = (By.XPATH, '//ul[1]/a[1]//p[contains(@class, "num")]')
     ORDER_BASKET = (By.XPATH, "//span[@class='constructor-element__text' and text()='Перетяните булочку сюда (низ)']")
     
-    # ЗАКАЗЫ - из вашего рабочего кода
+    # ЗАКАЗЫ
     CREATE_ORDER_BUTTON = (By.XPATH, '//button[text()="Оформить заказ"]')
     CLOSE_MODAL_ORDER = (By.XPATH, "//button[contains(@class, 'Modal_modal__close')][1]")
     ORDER_IDENTIFICATE = (By.XPATH, '//p[text()="идентификатор заказа"]')
