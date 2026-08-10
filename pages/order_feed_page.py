cfec6b57f827166ae95551d9f66aad93326c0183
Irina Kozlova <irina.kozlova@vk.team>
2026-08-10 23:51:33 +0300

    init.py files renamed, driver call in main_page.py fixed, @allure.step decorators added + allure-results commit

--- pages/order_feed_page.py
+++ pages/order_feed_page.py
@@ -1,12 +1,15 @@
 from pages.base_page import BasePage
 from locators.order_feed_locators import OrderFeedLocators
+import allure
 
 
 class OrderFeedPage(BasePage):
     
+    @allure.step('Клик по заказу в ленте')
     def click_order(self):
         self.click_on_element(OrderFeedLocators.ORDER_LINK)
     
+    @allure.step('Проверка структуры заказа')
     def check_order_structure(self):
         try:
             self.wait_until_element_visibility(OrderFeedLocators.ORDER_STRUCTURE)
@@ -14,6 +17,7 @@ class OrderFeedPage(BasePage):
         except:
             return False
     
+    @allure.step('Получение значения счетчика заказов')
     def get_total_order_count_daily(self, counter):
         try:
             self.wait_until_element_visibility(counter)
